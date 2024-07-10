2c1fbc952352311efde0c5d2af0b9c6d52f6fa3e
aidanaf <afarmayan@gmail.com>
2024-07-10 13:39:52 -0500

    Initial commit of project files

--- Agent/agent_config.py
+++ Agent/agent_config.py
@@ -6,4 +6,4 @@ DB_PASSWORD = "farmaaid000"
 DB_HOST = "localhost"
 DB_PORT = 5432
 
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = "sk-proj-2wRm9wojCyTX3G77payST3BlbkFJZdzcTRdkYXqbH2RsirBV"
