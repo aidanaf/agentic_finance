452b23f3c84379125a90152bff35f3e23198c3ee
aidanaf <afarmayan@gmail.com>
2024-07-10 13:49:23 -0500

    Initial commit of project files

---
+++ Agent/agent_config.py
@@ -0,0 +1,9 @@
+API_KEY = "578a2925fee546d9854f657a23ac9e10"
+DATABASE_URI = "postgresql+psycopg2://postgres:farmaaid000@localhost:5432/GetStocks"
+DB_NAME = "GetStocks"
+DB_USER = "postgres"
+DB_PASSWORD = "farmaaid000"
+DB_HOST = "localhost"
+DB_PORT = 5432
+
+OPENAI_API_KEY = "sk-proj-2wRm9wojCyTX3G77payST3BlbkFJZdzcTRdkYXqbH2RsirBV"
