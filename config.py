53fc68673aad06ac68a51cf59b4b0a59ad5c9ca2
aidanaf <afarmayan@gmail.com>
2024-07-10 13:45:23 -0500

    Remove sensitive files and update .gitignore

--- config.py
+++
@@ -1,7 +0,0 @@
-API_KEY = "578a2925fee546d9854f657a23ac9e10"
-DATABASE_URI = "postgresql+psycopg2://postgres:farmaaid000@localhost:5432/GetStocks"
-DB_NAME = "GetStocks"
-DB_USER = "postgres"
-DB_PASSWORD = "farmaaid000"
-DB_HOST = "localhost"
-DB_PORT = 5432
